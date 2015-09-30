f8337a7637314c30e26ab649fe206c74f3334bb1
Ming Xiang <mingxiangchan@gmail.com>
2015-09-30 12:23:21 +0800

    completed all tasks

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,11 +1,24 @@
+require_relative '../../config/environment'
+
 get '/' do
-  @aunty = params[:aunty]
-  puts @aunty
-  # Look in app/views/index.erb
+  @list = Url.list
+  puts "[LOG] Getting /"
+  puts "[LOG] Params: #{params.inspect}"
   erb :index
 end
 
-post '/aunty' do
-  speech = params[:user_input]
-  redirect to("/?aunty=#{speech}")
+post '/urls' do
+  # create a new Url
+  long_url = params[:long_url]
+  puts "DEBUG: long url is #{params[:long_url]}"
+  Url.shorten(long_url)
+  unique_key = Url.find_by(long_url: long_url).unique_key
+  redirect to("/")
+end
+
+  # i.e. /q6bda
+get '/:unique_key' do
+  # redirect to appropriate "long" URL
+  long_url = Url.find_by(unique_key: params[:unique_key]).long_url
+  redirect to("#{long_url}")
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,22 +1,32 @@
 class Url < ActiveRecord::Base
+  validates :long_url, uniqueness:true
 	validates :unique_key, uniqueness: true
   # Remember to create a migration!
-  def shorten
-  	Url.create!(long_url: long_url,unique_key: generate_unique_key)
+  def self.shorten(long_url)
+    #use find_or_initialize to confirm uniqueness of long_url
+  	row = Url.find_or_initialize_by(long_url: long_url)
+    #update unique key of row
+    row.update(unique_key: generate_unique_key)
   end
 
-  def generate_unique_key
-  	key = Array.new(n){[*"A".."Z", *"0".."9"].sample}.join
+  def self.generate_unique_key
+  	key = Array.new(6){[*"A".."Z", *"0".."9"].sample}.join
   	while list_all_unique_keys.include?(key)
-  		key = Array.new(n){[*"A".."Z", *"0".."9"].sample}.join 		
+  		key = Array.new(6){[*"A".."Z", *"0".."9"].sample}.join 		
   	end
   	key 
   end
 
-
-  def list_all_unique_keys
+  def self.list_all_unique_keys
   	list = []
   	Url.all.each{|row| list << row.unique_key}
   	list
+  end
+
+  def self.list
+    list = []
+    Url.all.each{|row| list << [row.long_url ,row.unique_key]}
+    puts "DEBUG #{list}"
+    list
   end
 end
