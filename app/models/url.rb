abfb8e0ecf76d1a0cb04593a7b13a5e54f803740
Ming Xiang <mingxiangchan@gmail.com>
2015-09-30 09:48:48 +0800

    created methods to create unique keys

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,22 @@
 class Url < ActiveRecord::Base
+	validates :unique_key, uniqueness: true
   # Remember to create a migration!
+  def shorten
+  	Url.create!(long_url: long_url,unique_key: generate_unique_key)
+  end
+
+  def generate_unique_key
+  	key = Array.new(n){[*"A".."Z", *"0".."9"].sample}.join
+  	while list_all_unique_keys.include?(key)
+  		key = Array.new(n){[*"A".."Z", *"0".."9"].sample}.join 		
+  	end
+  	key 
+  end
+
+
+  def list_all_unique_keys
+  	list = []
+  	Url.all.each{|row| list << row.unique_key}
+  	list
+  end
 end
--- db/migrate/20150930090742_create_urls.rb
+++ db/migrate/20150930090742_create_urls.rb
@@ -1,4 +1,10 @@
 class CreateUrls < ActiveRecord::Migration
   def change
+  	create_table :urls do |t|        
+      t.string :long_url
+      t.string :unique_key
+
+      t.timestamps null: false
+    end
   end
 end
